66c328b16492485a4fe89e1e56e1a0d42b046a40
Mohamed Barkaoui <mohamedbs1989@hotmail.com>
2024-05-06 16:02:11 +0300

    Add Statusbar for Consumer.

--- warehouse24/DataBase/ConsumerForm.Designer.cs
+++ warehouse24/DataBase/ConsumerForm.Designer.cs
@@ -45,9 +45,12 @@
             this.butSearchConsumer = new System.Windows.Forms.Button();
             this.consumerTableAdapter = new warehouse24.warehouse24DataSetTableAdapters.ConsumerTableAdapter();
             this.consumerTableAdapter1 = new warehouse24.warehouse24DataSetTableAdapters.ConsumerTableAdapter();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridConsumer)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.consumerBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.warehouse24DataSet)).BeginInit();
+            this.statusStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // butAddConsu
@@ -186,12 +189,29 @@
             // 
             this.consumerTableAdapter1.ClearBeforeFill = true;
             // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 457);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(546, 22);
+            this.statusStrip1.TabIndex = 13;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(118, 17);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
             // ConsumerForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(546, 467);
+            this.ClientSize = new System.Drawing.Size(546, 479);
+            this.Controls.Add(this.statusStrip1);
             this.Controls.Add(this.dataGridConsumer);
             this.Controls.Add(this.butSearchConsumer);
             this.Controls.Add(this.textBox2);
@@ -208,6 +228,8 @@
             ((System.ComponentModel.ISupportInitialize)(this.dataGridConsumer)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.consumerBindingSource)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.warehouse24DataSet)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -231,5 +253,7 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn idDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn nameDataGridViewTextBoxColumn;
         private warehouse24DataSetTableAdapters.ConsumerTableAdapter consumerTableAdapter1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
     }
 }
